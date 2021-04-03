5eede5f48f6490d15133b55bd3248ea9b148867e
Rintsen Sherpa <rintsen.sherpa@gmail.com>
2021-04-02 20:13:52 -0500

    fixed path to secrets file

--- qsCloneDB_proj/settings/prod.py
+++ qsCloneDB_proj/settings/prod.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from .base import *
 import json
 
-with open('etc/config.json') as config_file:
+with open('/etc/config.json') as config_file:
     config = json.load(config_file)
 
 SECRET_KEY = config['SECRET_KEY']
