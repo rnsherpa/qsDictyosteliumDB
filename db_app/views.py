f9831b77bb557a1d5040a70a634663d90f63721f
Rintsen Sherpa <rintsen.sherpa@gmail.com>
2021-05-07 12:19:59 -0500

    fixed filters to have OR conditions

--- db_app/views.py
+++ db_app/views.py
@@ -1,49 +1,56 @@
+from collections import defaultdict
 from distutils.util import strtobool
 from django.http import JsonResponse
 import csv
 from django.shortcuts import render
 from .models import Clone, Paper
-from django.db.models import Count, Q
+from django.db.models import Count, Q, query
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
     return render(request, 'db_app/home.html')
 
 def clones(request):
+    
     cloneModel = Clone.objects.all() # get all clone data
     
-    # these receive the filter entries
-    clone_id_query = request.GET.get('clone_search')
-    dicty_species_query = request.GET.get('dicty_species')
-    burk_species_query = request.GET.get('burk_species')
-    amo_endo_query = request.GET.get('amo_endo')
-    chlam_endo_query = request.GET.get('chlam_endo')
-    mating_type_query = request.GET.get('mating_type')
-    min_papers_query = request.GET.get('min_papers')
+    if request.method == 'GET':
+        # these receive the filter entries
+        clone_id_query = request.GET.get('clone_search')
+        dicty_species_query = request.GET.get('dicty_species') 
+        burk_species_query = request.GET.get('burk_species')
+        amo_endo_query = request.GET.get('amo_endo')
+        chlam_endo_query = request.GET.get('chlam_endo')
+        mating_type_query = request.GET.get('mating_type')
+        min_papers_query = request.GET.get('min_papers')
 
-    # we only want to filter if we are receiving filter entries
-    if clone_id_query !='' and clone_id_query is not None:
-        cloneModel = cloneModel.filter(Q(qs_id__iexact=clone_id_query) | Q(alt_id__iexact=clone_id_query))
-    
-    if dicty_species_query !='' and dicty_species_query is not None:
-        cloneModel = cloneModel.filter(species=dicty_species_query)
-    
-    if burk_species_query !='' and burk_species_query is not None:
-        cloneModel = cloneModel.filter(burk_species=burk_species_query)
+        # we only want to filter if we are receiving filter entries
+        if clone_id_query !='' and clone_id_query is not None:
+            cloneModel = cloneModel.filter(Q(qs_id__iexact=clone_id_query) | Q(alt_id__iexact=clone_id_query))
+        
+        if dicty_species_query !='' and dicty_species_query is not None:
+            dicty_species_query = request.GET.getlist('dicty_species') # getlist for requests that can have multiple options in them
+            cloneModel = cloneModel.filter(species__in=dicty_species_query) # __in is used for mulitple "or" conditions
+        
+        if burk_species_query !='' and burk_species_query is not None:
+            burk_species_query = request.GET.getlist('burk_species')
+            cloneModel = cloneModel.filter(burk_species__in=burk_species_query)
 
-    if amo_endo_query !='' and amo_endo_query is not None:
-        amo_bool = strtobool(amo_endo_query) # the html form sends over "True" or "False" as strings, so we need to convert them into booleans
-        cloneModel = cloneModel.filter(is_amo=amo_bool)
-    
-    if chlam_endo_query!='' and chlam_endo_query is not None:
-        chlam_bool = strtobool(chlam_endo_query)
-        cloneModel = cloneModel.filter(is_chlam=chlam_bool)
-    
-    if mating_type_query!='' and mating_type_query is not None:
-        cloneModel = cloneModel.filter(mating_type=int(mating_type_query))
+        if amo_endo_query !='' and amo_endo_query is not None:
+            amo_bool = strtobool(amo_endo_query) # the html form sends over "True" or "False" as strings, so we need to convert them into booleans
+            cloneModel = cloneModel.filter(is_amo=amo_bool)
+        
+        if chlam_endo_query!='' and chlam_endo_query is not None:
+            chlam_bool = strtobool(chlam_endo_query)
+            cloneModel = cloneModel.filter(is_chlam=chlam_bool)
+        
+        if mating_type_query!='' and mating_type_query is not None:
+            mating_type_query = request.GET.getlist('mating_type')
+            mating_type_query = [int(x) for x in mating_type_query] # convert all items in list to string
+            cloneModel = cloneModel.filter(mating_type__in=mating_type_query)
 
-    if min_papers_query!='' and min_papers_query is not None:
-        cloneModel = cloneModel.annotate(num_papers=Count('paper')).filter(num_papers__gte=int(min_papers_query)).order_by('index')
+        if min_papers_query!='' and min_papers_query is not None:
+            cloneModel = cloneModel.annotate(num_papers=Count('paper')).filter(num_papers__gte=int(min_papers_query)).order_by('index')
 
     page = request.GET.get('page', 1)
     paginator = Paginator(cloneModel, 10) # 10 clones per page
