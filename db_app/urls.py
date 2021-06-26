29aeaec8c258a300f4badb1e14daf1e2d7f2b0e4
Rintsen Sherpa <rintsen.sherpa@gmail.com>
2021-06-26 11:48:57 -0400

    changed csv export from js to backend

--- db_app/urls.py
+++ db_app/urls.py
@@ -3,10 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='db-home'),
-    path('clones/', views.clones, name='db-clones'),
-    path('papers/', views.papers, name='db-papers'),
     path('about/', views.about, name='db-about'),
-    path('clones/<str:qs_id>/', views.dynamic_clone_view, name='clone-details'), #clone url generation
-    path('papers/<str:index>/', views.dynamic_paper_view, name='paper-details'), #clone url generation
-    path('counts-chart/', views.counts_chart, name='counts-chart'),
+    
+    path('clones/', views.clones, {"exportCSV": False}, name='db-clones'),
+    path('clones/export_csv/', views.clones, {"exportCSV": True}, name='clones-csv'), #export url for main clones page
+    path('clones/<str:qs_id>/', views.dynamic_clone_view, {"exportCSV": False}, name='clone-details'), #clone url generation
+    path('clones/<str:qs_id>/export_csv/', views.dynamic_clone_view, {"exportCSV": True}, name='papers-using-clone-csv'), #export url for papers of a clone
+    
+    path('papers/', views.papers, {"exportCSV": False}, name='db-papers'),
+    path('papers/export_csv/', views.papers, {"exportCSV": True}, name='papers-csv'), #export url for main papers page
+    path('papers/<str:index>/', views.dynamic_paper_view, {"exportCSV": False}, name='paper-details'), #clone url generation
+    path('papers/<str:index>/export_csv', views.dynamic_paper_view, {"exportCSV": True}, name='clones-in-paper-csv'), #export url for clones in a paper
+    
+    path('counts_chart/', views.counts_chart, name='counts-chart'),
 ]
--- db_app/views.py
+++ db_app/views.py
@@ -1,7 +1,7 @@
-from collections import defaultdict
 from distutils.util import strtobool
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import csv
+from datetime import date
 from django.shortcuts import render
 from .models import Clone, Paper
 from django.db.models import Count, Q, query
@@ -10,7 +10,21 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def home(request):
     return render(request, 'db_app/home.html')
 
-def clones(request):
+def counts_chart(request):
+    labels = []
+    data = []
+
+    queryset = Clone.objects.annotate(num_papers=Count('paper')).order_by('-num_papers')[:50]
+    for entry in queryset:
+        labels.append(entry.qs_id)
+        data.append(entry.num_papers)
+
+    return JsonResponse(data={'labels': labels,'data': data,})
+
+def about(request):
+    return render(request, "db_app/about.html")
+
+def clones(request, exportCSV):
     
     cloneModel = Clone.objects.all() # get all clone data
     
@@ -52,22 +66,26 @@ def clones(request):
         if min_papers_query!='' and min_papers_query is not None:
             cloneModel = cloneModel.annotate(num_papers=Count('paper')).filter(num_papers__gte=int(min_papers_query)).order_by('index')
 
-    page = request.GET.get('page', 1)
-    paginator = Paginator(cloneModel, 10) # 10 clones per page
-    try:
-        clones = paginator.page(page)
-    except PageNotAnInteger:
-        clones = paginator.page(1)
-    except EmptyPage:
-        clones = paginator.page(paginator.num_pages)
+    if exportCSV:
+        return export_clones_csv(cloneModel)
 
-    speciesList = Clone.objects.order_by('species').values('species').distinct() # get a list of the species from the Clone model
-    burkList = Clone.objects.order_by('burk_species').values('burk_species').distinct().exclude(burk_species__exact='')
+    else:       
+        page = request.GET.get('page', 1)
+        paginator = Paginator(cloneModel, 10) # 10 clones per page
+        try:
+            clones = paginator.page(page)
+        except PageNotAnInteger:
+            clones = paginator.page(1)
+        except EmptyPage:
+            clones = paginator.page(paginator.num_pages)
 
-    context = {'clones': clones, 'speciesList': speciesList, 'burkList': burkList}
-    return render(request, 'db_app/clones.html', context)
+        speciesList = Clone.objects.order_by('species').values('species').distinct() # get a list of the species from the Clone model
+        burkList = Clone.objects.order_by('burk_species').values('burk_species').distinct().exclude(burk_species__exact='')
+
+        context = {'clones': clones, 'speciesList': speciesList, 'burkList': burkList}
+        return render(request, 'db_app/clones.html', context)
     
-def papers(request):
+def papers(request, exportCSV):
     paperModel = Paper.objects.all()
 
     if request.method == 'GET':
@@ -77,58 +95,84 @@ def papers(request):
         if paper_query !='' and paper_query is not None:
             paperModel = paperModel.filter(Q(title__icontains=paper_query) | Q(first_author__icontains=paper_query))
 
-    page = request.GET.get('page', 1)
-    paginator = Paginator(paperModel, 10) # 10 clones per page
-    try:
-        papers = paginator.page(page)
-    except PageNotAnInteger:
-        papers = paginator.page(1)
-    except EmptyPage:
-        papers = paginator.page(paginator.num_pages)
+    if exportCSV:
+        return export_papers_csv(paperModel)    
+        
+    else:      
+        page = request.GET.get('page', 1)
+        paginator = Paginator(paperModel, 10) # 10 clones per page
+        try:
+            papers = paginator.page(page)
+        except PageNotAnInteger:
+            papers = paginator.page(1)
+        except EmptyPage:
+            papers = paginator.page(paginator.num_pages)
 
-    context = {'papers': papers}
-    return render(request, 'db_app/papers.html', context)
+        context = {'papers': papers}
+        return render(request, 'db_app/papers.html', context)
 
-def dynamic_clone_view(request, qs_id):
+def dynamic_clone_view(request, qs_id, exportCSV):
     clone = Clone.objects.get(qs_id__iexact=qs_id) #__iexact: removes case sensitivity
     papers_containing_clone = clone.paper_set.all() #set of papers that have used a given clone
-    page = request.GET.get('page', 1)
-    paginator = Paginator(papers_containing_clone, 10) # 10 papers per page
-    try:
-        papers = paginator.page(page)
-    except PageNotAnInteger:
-        papers = paginator.page(1)
-    except EmptyPage:
-        papers = paginator.page(paginator.num_pages)
+        
+    if exportCSV:
+        return export_papers_csv(papers_containing_clone)
 
-    context = {'clone': clone, 'papers': papers}
-    return render(request, "db_app/clone_details.html", context)
+    else:        
+        page = request.GET.get('page', 1)
+        paginator = Paginator(papers_containing_clone, 10) # 10 papers per page
+        try:
+            papers = paginator.page(page)
+        except PageNotAnInteger:
+            papers = paginator.page(1)
+        except EmptyPage:
+            papers = paginator.page(paginator.num_pages)
 
-def dynamic_paper_view(request, index):
+        context = {'clone': clone, 'papers': papers}
+        return render(request, "db_app/clone_details.html", context)
+
+def dynamic_paper_view(request, index, exportCSV):
     paper = Paper.objects.get(index=index)
     clones_from_paper = paper.clones.all() #set of clones used in a given paper
-    page = request.GET.get('page', 1)
-    paginator = Paginator(clones_from_paper, 10) # 10 clones per page
-    try:
-        clones = paginator.page(page)
-    except PageNotAnInteger:
-        clones = paginator.page(1)
-    except EmptyPage:
-        clones = paginator.page(paginator.num_pages)
+    
+    if exportCSV:
+        return export_clones_csv(clones_from_paper)
 
-    context = {'paper': paper, 'clones': clones}
-    return render(request, "db_app/paper_details.html", context)
+    else:    
+        page = request.GET.get('page', 1)
+        paginator = Paginator(clones_from_paper, 10) # 10 clones per page
+        try:
+            clones = paginator.page(page)
+        except PageNotAnInteger:
+            clones = paginator.page(1)
+        except EmptyPage:
+            clones = paginator.page(paginator.num_pages)
 
-def counts_chart(request):
-    labels = []
-    data = []
+        context = {'paper': paper, 'clones': clones}
+        return render(request, "db_app/paper_details.html", context)
 
-    queryset = Clone.objects.annotate(num_papers=Count('paper')).order_by('-num_papers')[:50]
-    for entry in queryset:
-        labels.append(entry.qs_id)
-        data.append(entry.num_papers)
+def export_clones_csv(queryset):
+    
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;filename=qsdictyclones_{}.csv'.format(date.today())
+    response.write(u'\ufeff'.encode('utf8'))
+    writer = csv.writer(response)
+    csv_header = ['QS ID', 'Alternate ID', 'Species', 'Origin Location', 'Origin Coordinates', 'Box Plated From', 'Date Collected', 'Date Frozen', ' Collection Method', 'Culture Method', 'Origin Soil Sample', 'Location in Freezer', 'Box Name', 'Box Initials', 'Is Clonal', 'Mating Type', 'viol.hap.grp', 'Sequenced', 'Amoebophilis Endosymbiont', 'Neochlamydia Endosymbiont', 'Neochlamydia Haplotype', 'Endosymbiont Haplotype ID', 'Endosymbiont 16S Sequence Hit', 'Paraburkholderia Species']
+    writer.writerow(csv_header)
+    for item in queryset:
+        writer.writerow([item.qs_id, item.alt_id, item.species, item.origin, item.origin_coords, item.box_plated_from, item.collect_date, item.freeze_date, item.collect_method, item.culture_method, item.origin_soil_sample, item.freezer_loc, item.box_name, item.box_initials, item.is_clonal, item.mating_type, item.viol_hap_grp, item.sequenced, item.is_amo, item.is_chlam, item.chlam_hap, item.endo_hap_id, item.endo_seq_hit, item.burk_species])
+    
+    return response
 
-    return JsonResponse(data={'labels': labels,'data': data,})
+def export_papers_csv(queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;filename=qs_papers_{}.csv'.format(date.today())
+    response.write(u'\ufeff'.encode('utf8'))
+    writer = csv.writer(response)
+    csv_header = ['DOI', 'Title', 'First Author', 'Last Author', 'Journal', 'Year']
+    writer.writerow(csv_header)
+    for item in queryset:
+        writer.writerow([item.doi, item.title, item.first_author, item.last_author, item.journal, item.year])
 
-def about(request):
-    return render(request, "db_app/about.html")
+    return response
+
